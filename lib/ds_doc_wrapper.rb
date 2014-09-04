e8620691d156fa7b3425d34b378bcd66f6314262
nardele salomon <noliveira@telelistas.net>
2014-09-04 17:12:53 -0300

    fix require path

--- lib/ds_doc_wrapper.rb
+++ lib/ds_doc_wrapper.rb
@@ -1,4 +1,4 @@
-
+$:.push File.expand_path("../lib", __FILE__) unless $:.include? File.expand_path("../lib", __FILE__)
 require 'doc_wrapper/base'
 module DocWrapper
 end
--- test/dummy/config/application.rb
+++ test/dummy/config/application.rb
@@ -3,7 +3,7 @@ require File.expand_path('../boot', __FILE__)
 require 'rails/all'
 
 Bundler.require(*Rails.groups)
-require "doc_wrapper"
+require "ds_doc_wrapper"
 
 module Dummy
   class Application < Rails::Application
